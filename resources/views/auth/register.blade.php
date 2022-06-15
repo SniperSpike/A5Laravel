c169dcb96e3c59e3f99ae59a45c1ac4f8e0d48e2
SniperSpike <joyvroolanddnv@gmail.com>
2022-06-15 20:45:23 +0200

    quick fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,69 +5,67 @@
     <div class="d-flex justify-content-center">
         <div class="col-md-3">
             <div class="registerForm">
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
 
-                        <h2>Meld je aan</h2>
-                        <div class="mb-1">
-                            <label for="name" class="col-form-label">{{ __('Gebruikersnaam') }}</label>
+                    <h2>Meld je aan</h2>
+                    <div class="mb-1">
+                        <label for="name" class="col-form-label">{{ __('Gebruikersnaam') }}</label>
 
-                            <div class="">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        <div class="">
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="mb-1">
-                            <label for="email" class="col-form-label">{{ __('Email') }}</label>
+                    <div class="mb-1">
+                        <label for="email" class="col-form-label">{{ __('Email') }}</label>
 
-                            <div class="">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        <div class="">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="mb-1">
-                            <label for="password" class="col-form-label">{{ __('Wachtwoord') }}</label>
+                    <div class="mb-1">
+                        <label for="password" class="col-form-label">{{ __('Wachtwoord') }}</label>
 
-                            <div class="">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                        <div class="">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="mb-1">
-                            <label for="password-confirm" class="col-form-label">{{ __('Wachtwoord bevestigen') }}</label>
+                    <div class="mb-1">
+                        <label for="password-confirm" class="col-form-label">{{ __('Wachtwoord bevestigen') }}</label>
 
-                            <div>
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                        <div>
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                         </div>
+                    </div>
 
-                        <div class="mt-4">
-                            <button type="submit" class="btn btn-RegisterForm">
-                                {{ __('Aanmelden') }}
-                            </button>
-                        </div>
-              
-                    </form>
-                </div>
+                    <div class="mt-4">
+                        <button type="submit" class="btn btn-RegisterForm">
+                            {{ __('Aanmelden') }}
+                        </button>
+                    </div>
+            
+                </form>
             </div>
         </div>
     </div>
