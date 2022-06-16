d436311a7753ac32ba6be3105005975a48e2b4bc
NielsRol97 <rolniels@gmail.com>
2022-06-16 20:01:12 +0200

    Nog meer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,15 +11,22 @@
             <h1>Over EPK Media</h1>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tellus ligula, gravida ut velit eu, molestie interdum lacus. Proin in lacus risus. Phasellus odio est, venenatis in orci non, aliquam congue lacus. Aliquam et nisi eu urna consectetur malesuada. In hac habitasse platea dictumst. Nam commodo nibh vel elit molestie, ac malesuada mauris congue. Donec congue nibh justo, in porta ex finibus vel. Cras purus augue, mattis eget justo non, fermentum fermentum nulla. Vivamus vestibulum, urna ut aliquet varius, elit est consectetur neque, in molestie nisi ex eget libero. Integer molestie lorem vitae ante semper, id accumsan ligula rhoncus. Vivamus rhoncus nibh nulla, sit amet scelerisque orci facilisis et. Nulla velit neque, dictum vel gravida sit amet, dignissim quis massa. Etiam elementum, diam eget congue finibus, augue dui rhoncus lorem, sodales accumsan urna metus pulvinar risus.
-                Donec non dignissim libero. Morbi bibendum lacinia purus, id semper urna volutpat a. Nullam non egestas massa. Pellentesque eget maximus lorem. Morbi justo elit, mattis at dignissim non, euismod non ligula. Pellentesque risus lorem, fermentum sit amet velit ac, blandit eleifend dui. Maecenas hendrerit ac felis a molestie. Nullam finibus pharetra neque sit amet fringilla. Mauris id lacus a massa bibendum iaculis. Sed nisi enim, ullamcorper id dictum venenatis, suscipit a justo. Morbi mattis sed elit in cursus. Phasellus dictum ex a arcu pellentesque, sed gravida libero viverra. Mauris consectetur erat a tortor aliquam fermentum.  
-                Aenean suscipit laoreet erat, ut auctor justo iaculis quis. Nam ex ligula, luctus sed risus at, molestie aliquet massa. Duis nec semper diam. Morbi mi lectus, rhoncus eget nulla facilisis, mollis fringilla felis. Fusce ut commodo sapien. Vivamus tincidunt faucibus nisl et vestibulum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus non lobortis quam. Donec viverra ex eget condimentum dapibus. Donec condimentum felis eget enim consequat.
             </p>
         </article>
         <img src="{{asset('images/about.svg')}}" alt="">
     </div>
     <div class="inspiratie-wrapper">
-        <div class="tiles">
-            
+        <div class="tiles-container">
+            <img src="{{asset('images/inspiratie.svg')}}" alt="">
+            <img src="{{asset('images/inspiratie.svg')}}" alt="">
+            <img src="{{asset('images/inspiratie.svg')}}" alt="">
+            <img src="{{asset('images/inspiratie.svg')}}" alt="">
+            <img src="{{asset('images/inspiratie.svg')}}" alt="">
+            <img src="{{asset('images/inspiratie.svg')}}" alt="">
+            <img src="{{asset('images/inspiratie.svg')}}" alt="">
+            <img src="{{asset('images/inspiratie.svg')}}" alt="">
+            <img src="{{asset('images/inspiratie.svg')}}" alt="">
+            <img src="{{asset('images/inspiratie.svg')}}" alt="">
         </div>
     </div>
 </div>
