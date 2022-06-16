11e6bada51d6187787bf72754daa41a71d348296
SniperSpike <joyvroolanddnv@gmail.com>
2022-06-16 21:04:41 +0200

    verkenner searchbar css

--- resources/views/verkennen.blade.php
+++ resources/views/verkennen.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 <div class="container  col-sm-12 verkennenWrapper">
-
+    <div class="searchBar">
+        <input type="text" placeholder="Zoek door onze bibliotheek...">
+        <button>
+            <img src="{{asset('images/search.svg')}}" />
+        </button>
+    </div>
 </div>
 @endsection
