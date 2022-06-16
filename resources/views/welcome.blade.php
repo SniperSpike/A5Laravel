d290034a837c0a6415ef9b68c587d50e848496f2
SniperSpike <joyvroolanddnv@gmail.com>
2022-06-17 01:46:51 +0200

    small fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,9 @@
         <article>
             <h1>Over EPK Media</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tellus ligula, gravida ut velit eu, molestie interdum lacus. Proin in lacus risus. Phasellus odio est, venenatis in orci non, aliquam congue lacus. Aliquam et nisi eu urna consectetur malesuada. In hac habitasse platea dictumst. Nam commodo nibh vel elit molestie, ac malesuada mauris congue. Donec congue nibh justo, in porta ex finibus vel. Cras purus augue, mattis eget justo non, fermentum fermentum nulla. Vivamus vestibulum, urna ut aliquet varius, elit est consectetur neque, in molestie nisi ex eget libero. Integer molestie lorem vitae ante semper, id accumsan ligula rhoncus. Vivamus rhoncus nibh nulla, sit amet scelerisque orci facilisis et. Nulla velit neque, dictum vel gravida sit amet, dignissim quis massa. Etiam elementum, diam eget congue finibus, augue dui rhoncus lorem, sodales accumsan urna metus pulvinar risus.
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.
+                <br><br>
+                eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem . 
             </p>
         </article>
         <img src="{{asset('images/about.svg')}}" alt="">
