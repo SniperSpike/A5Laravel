b7291d9767ef00d10c487b2103ee850be4e713bd
NielsRol97 <rolniels@gmail.com>
2022-06-16 12:09:20 +0200

    Homepagina aanpassingen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,12 @@
 
 @section('homecontent')
 <div class="homecontentwrapper">
-
     <div class="personalised">
-
+        <img src="{{asset('images/personalised.svg')}}" alt="">
+        <button>Aan de slag</button>
     </div>
-
     <div class="about">
-        <h1>Over EPK Media</h1>
-        <article>    
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit, massa id tristique commodo, orci sem eleifend ligula, a pellentesque neque sapien ac lorem. Fusce nec ipsum sagittis, dignissim lectus at, egestas nulla. Vivamus pulvinar arcu sed luctus ornare. Donec placerat, diam in mattis iaculis, eros purus aliquet velit, vitae volutpat purus nibh in arcu. Nam id venenatis ante, in ornare felis. In fringilla nisi sit amet erat convallis, nec laoreet risus tristique. Vivamus porttitor turpis ut convallis consectetur. Integer libero orci, malesuada sit amet volutpat id, semper ut lorem. Suspendisse pellentesque leo lacus. Mauris imperdiet pellentesque felis ac bibendum.
-        </article>
-    </div>
-
-    <div class="inspiration">
 
     </div>
-
 </div>
 @endsection
