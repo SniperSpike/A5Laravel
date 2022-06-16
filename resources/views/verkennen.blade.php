cbe09ecc129fc872b64ea192c49638c2281ab90c
SniperSpike <joyvroolanddnv@gmail.com>
2022-06-16 21:24:13 +0200

    verkenner update

--- resources/views/verkennen.blade.php
+++ resources/views/verkennen.blade.php
@@ -8,5 +8,17 @@
             <img src="{{asset('images/search.svg')}}" />
         </button>
     </div>
+    <h3>Onze bibliotheek</h3>
+    <div class="verkenItems">
+        @for ($i = 0; $i < 10; $i++)
+            <a href="#">
+                <div class="itemContainer">
+                    <div class="item">
+                    </div>
+                    <p>AC/DC</p>
+                </div>
+            </a>
+        @endfor
+    </div>
 </div>
 @endsection
