88eb6b3f09f4a2112716c649cfa7962e2407c5a3
SniperSpike <joyvroolanddnv@gmail.com>
2022-06-21 22:20:58 +0200

    small fix

--- resources/views/auth/edit.blade.php
+++ resources/views/auth/edit.blade.php
@@ -152,7 +152,7 @@
                         <div class="modal fade" id="exampleModal" data-bs-backdrop="static" data-bs-keyboard="false"
                             tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   
-                                <div class="modal-content modal-dialog modal-lg">
+                                <div class="modal-content modal-dialog modal-xl">
                                     <div class="modal-header">
                                         <h5 class="modal-title" id="exampleModalLabel">Band banner croppen</h5>
                                         <button type="button" class="btn-close" data-bs-dismiss="modal"
